747b5f7e3fc722cf9e6877ebffc31c2bb14283d8
hexzhou <hexzhou@hotmail.com>
2015-05-10 16:06:08 +0800

    hex add code for fibonacci

---
+++ training/hex/fibonacci/fibonacci.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- encoding=utf8 -*-
+
+import sys
+
+if __name__ == '__main__':
+	fib = [0,1,]
+	for x in xrange(2,100):
+		fib.append(fib[x-1] + fib[x-2])
+
+	while True:
+		line = sys.stdin.readline()
+		if not line:
+			break
+		n = int(line.strip())
+		print fib[n]
