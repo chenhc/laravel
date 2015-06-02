99c229339e7a751dd2d77212e3328c682b9bea90
dddq39 <18819451697@163.com>
2015-06-02 11:59:09 +0800

    add a spider which crawl category lists and store dict(category, material)

--- crawler/crawler/pipelines.py
+++ crawler/crawler/pipelines.py
@@ -8,13 +8,14 @@
 import json
 from scrapy import log
 
-from crawler.items import FoodMaterialItem
+from crawler.items import FoodMaterialItem, Category_Material
 
 
 class FileStorePipeline(object):
 
     def __init__(self):
         self.food_material = file('/tmp/food_material', 'w+')
+        self.category_material = file('/tmp/category_material','w+')
 
     def store_into(self, item, f):
         f.write(json.dumps(dict(item)))
@@ -26,6 +27,10 @@ class FileStorePipeline(object):
             log.msg('[STORE][file][food_material] name=%s category=%s' %
                     (item['name'], item['category']), level=log.INFO)
             self.store_into(item, self.food_material)
+        if isinstance(item, Category_Material):
+            log.msg('[STORE][file][category_material] category=%s material=%s' %
+                    ( item['category'], item['material']), level=log.INFO)
+            self.store_into(item, self.category_material)
         return item
 
 
