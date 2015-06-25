6845dd9520b08ac96b349530307f7ff55470ba24
Fasion Chan <fasionchan@gmail.com>
2015-06-25 21:44:01 +0800

    add address schema

--- database/sql/ilive_schema.sql
+++ database/sql/ilive_schema.sql
@@ -8,6 +8,57 @@
 -- Changelog:
 --
 --
+-- 国家
+CREATE TABLE `country` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `country` VARCHAR(255) NOT NULL,
+    KEY (`country`),
+    PRIMARY KEY(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- 省份
+CREATE TABLE `province` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `province` VARCHAR(255) NOT NULL,
+    `country_id` INTEGER NOT NULL,
+    KEY (`province`),
+    KEY `country_id` (`country_id`),
+    CONSTRAINT `prvc_country_id_fk` FOREIGN KEY (`country_id`) REFERENCES `country` (`id`),
+    PRIMARY KEY(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- 城市
+CREATE TABLE `city` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `city` VARCHAR(255) NOT NULL,
+    `province_id` INTEGER NOT NULL,
+    KEY (`city`),
+    KEY `province_id` (`province_id`),
+    CONSTRAINT `ct_province_id_fk` FOREIGN KEY (`province_id`) REFERENCES `province` (`id`),
+    PRIMARY KEY(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- 区
+CREATE TABLE `district` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `district` VARCHAR(255) NOT NULL,
+    `city_id` INTEGER NOT NULL,
+    KEY (`district`),
+    KEY `city_id` (`city_id`),
+    CONSTRAINT `dst_city_id_fk` FOREIGN KEY (`city_id`) REFERENCES `city` (`id`),
+    PRIMARY KEY(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- 街道
+CREATE TABLE `street` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `street` VARCHAR(255) NOT NULL,
+    `district_id` INTEGER NOT NULL,
+    KEY (`street`),
+    KEY `district_id` (`district_id`),
+    CONSTRAINT `st_district_id_fk` FOREIGN KEY (`district_id`) REFERENCES `district` (`id`),
+    PRIMARY KEY(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 -- 用户
