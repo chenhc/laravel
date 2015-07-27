42411065023964c8b42c25a53a3219e1f53416d8
hexzhou <hexzhou@hotmail.com>
2015-07-28 00:26:36 +0800

    add test case for food_material

--- website/database/factories/ModelFactory.php
+++ website/database/factories/ModelFactory.php
@@ -20,3 +20,22 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\FoodMaterial::class, function (Faker\Generator $faker)
+{
+    return [
+        'name' => $faker->name,
+        'hash' => str_random(16),
+        'classification' => '速食食品',
+        'category' => '方便食品类',
+        'alias' => $faker->name,
+        'tags' => $faker->name,
+        'image_hash' => str_random(16),
+    ];
+});
+
+$factory->define(App\FoodRecipe::class, function (Faker\Generator $facker)
+{
+    return [
+    ];
+});
